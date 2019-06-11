ae50f5695317c05560359f29c29fa0e0ea2a9744
Jeevan Kishore <kishor080@gmail.com>
2019-06-11 15:14:41 +0530

    feat(knobs): Implement knobs :construction_worker:

--- components/atoms/headline/stories.js
+++ components/atoms/headline/stories.js
@@ -2,9 +2,18 @@ import React from "react";
 import { storiesOf } from '@storybook/react';
 import Headline from "./index";
 import { generateStory } from "../fixture/index";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
 
 const { story } = generateStory();
+const storyData = storiesOf("Atoms/Headline");
+storyData.addDecorator(withKnobs);
 
-storiesOf("Atoms/Headline")
-  .add("default", () => <Headline text={story.headline} headerType={1} headerLevel={2} />)
+storyData.add("default", () => {
+  const rowData = {
+    background: boolean("Background dark", false)
+  };
+
+  return <Headline text={story.headline} headerType={1} headerLevel={2} {...rowData}/>
+})
   .add("Collection Name", () => <Headline text={story.headline} headerType={8} headerLevel={1} />);
+
