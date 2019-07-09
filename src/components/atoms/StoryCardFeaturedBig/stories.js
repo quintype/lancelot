07954f67b6218c5f3145caedc97acba7f4fc444f
Nandakishore Prakash Rao <nandap4790@gmail.com>
2019-07-09 17:15:34 +0530

    feat(four-story-half-featured): component

---
+++ src/components/atoms/StoryCardFeaturedBig/stories.js
@@ -0,0 +1,72 @@
+import React from "react";
+import produce from "immer";
+import props from "./fixture.js";
+
+import StoryCardFeaturedBig from "./";
+import { withStore } from "../../../../storybook";
+
+withStore("Cards/StoryCardFeaturedBig", {
+  qt: {
+    config: {
+      "cdn-image": "thumbor-stg.assettype.com",
+      iconSpritePath: "/sprite.svg"
+    }
+  }
+})
+  .addDecorator(story => <div style={{ maxWidth: 700 }}>{story()}</div>)
+  .add("Default", () => <StoryCardFeaturedBig {...props} />)
+  .add("when headline is not present", () => {
+    const newProps = produce(props, draft => {
+      draft.story.headline = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("when slug is not present", () => {
+    const newProps = produce(props, draft => {
+      draft.story.url = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("when author name is not present", () => {
+    const newProps = produce(props, draft => {
+      draft.story["authors"] = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("when section color is not present", () => {
+    const newProps = produce(props, draft => {
+      draft.story.sections[0].color = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("missing hero image", () => {
+    const newProps = produce(props, draft => {
+      draft.story["hero-image-s3-key"] = undefined;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("missing alternate image", () => {
+    const newProps = produce(props, draft => {
+      draft.story.alternative.home.default["hero-image"]["hero-image-s3-key"] = undefined;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("missing hero image, alternate image", () => {
+    const newProps = produce(props, draft => {
+      draft.story["hero-image-s3-key"] = null;
+      draft.story.alternative.home.default["hero-image"]["hero-image-s3-key"] = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("should not show section if not present", () => {
+    const newProps = produce(props, draft => {
+      draft.story["sections"] = null;
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  })
+  .add("premium story", () => {
+    const newProps = produce(props, draft => {
+      draft.story.access = "subscription";
+    });
+    return <StoryCardFeaturedBig {...newProps} />;
+  });
