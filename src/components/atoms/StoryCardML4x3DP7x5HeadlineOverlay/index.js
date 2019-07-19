67674dbbd59f0de7916a3c9752f3c6a41e16324b
Nandakishore Prakash Rao <nandap4790@gmail.com>
2019-07-19 12:09:23 +0530

    feat(5s1a1b): component

---
+++ src/components/atoms/StoryCardML4x3DP7x5HeadlineOverlay/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+import PropTypes from "prop-types";
+import get from "lodash/get";
+import { Link } from "@quintype/components";
+
+import Contributor from "../Contributor";
+import ResponsiveImageWithFallback from "../ResponsiveImageWithFallback";
+import { getStoryData, generateImageSources } from "../../utils/utils";
+
+import "./storyCardML4x3DP7x5HeadlineOverlay.m.css";
+import Headline from "../Headline";
+
+export const StoryCardML4x3DP7x5HeadlineOverlay = ({ story, className = "", cardWithImageZoom = true }) => {
+  const storyData = getStoryData(story);
+
+  if (!(storyData.headline && story.url)) {
+    return null;
+  }
+
+  const contributorName = get(story, ["authors", 0, "name"], null);
+  const contributorRole = get(story, ["authors", 0, "contributor-role", "name"], "");
+  const externalLink = get(story, ["metadata", "reference-url"]);
+
+  return (
+    <Link
+      aria-label={`${"Read full story: "} ${storyData.headline}`}
+      href={externalLink || story.url}
+      externalLink={externalLink}
+      styleName="base"
+      className={`${className} ${cardWithImageZoom ? "card-with-image-zoom" : ""}`}
+    >
+      <div styleName="image-container">
+        <ResponsiveImageWithFallback
+          styleName="image-wrapper"
+          slug={storyData.imageS3Key}
+          metadata={storyData.imageMetadata}
+          alt={storyData.imageCaption}
+          imgParams={{ auto: ["format", "compress"] }}
+          sources={generateImageSources(
+            {
+              aspectRatio: [4, 3],
+              screenWidthCoverage: 0.3
+            },
+            {
+              aspectRatio: [7, 5],
+              screenWidthCoverage: 0.4
+            }
+          )}
+        />
+      </div>
+      <div styleName="text-wrapper">
+        {contributorName && <Contributor name={contributorName} type={contributorRole} className="contributor" />}
+        <Headline text={storyData.headline} headerType={6} headerLevel={3} styleName="headline" />
+      </div>
+    </Link>
+  );
+};
+
+StoryCardML4x3DP7x5HeadlineOverlay.propTypes = {
+  className: PropTypes.string,
+  cardWithImageZoom: PropTypes.bool,
+  story: PropTypes.shape({
+    id: PropTypes.string,
+    authors: PropTypes.arrayOf(
+      PropTypes.shape({
+        "contributor-role": PropTypes.shape({
+          name: PropTypes.string
+        }),
+        name: PropTypes.string
+      })
+    ),
+    headline: PropTypes.string.isRequired,
+    metadata: PropTypes.shape({
+      "reference-url": PropTypes.string
+    }),
+    alternative: PropTypes.shape({
+      home: PropTypes.shape({
+        default: PropTypes.shape({
+          headline: PropTypes.string
+        })
+      })
+    }),
+    slug: PropTypes.string.isRequired,
+    "hero-image-metadata": PropTypes.shape({
+      height: PropTypes.number,
+      width: PropTypes.number,
+      "aspect-ratio": PropTypes.arrayOf(PropTypes.number)
+    }),
+    "hero-image-s3-key": PropTypes.string,
+    "hero-image-caption": PropTypes.string
+  })
+};
+
+export default StoryCardML4x3DP7x5HeadlineOverlay;
---
+++ src/components/rows/FiveStoriesOneAdBundle/stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import produce from "immer";
+
+import { withStore } from "../../../../storybook";
+import props from "./fixture.js";
+
+import FiveStoriesOneAdBundle from ".";
+
+withStore("Rows/FiveStoriesOneAdBundle", {
+  qt: {
+    config: {
+      "cdn-image": "thumbor-stg.assettype.com",
+      iconSpritePath: "/sprite.svg"
+    }
+  }
+})
+  .add("Default", () => <FiveStoriesOneAdBundle {...props} />)
+  .add("when less stories(3) are present", () => {
+    const newProps = produce(props, draft => {
+      draft.collection.items = draft.collection.items.slice(0, 3);
+    });
+    return <FiveStoriesOneAdBundle {...newProps} />;
+  })
+  .add("when story headline is missing", () => {
+    const newProps = produce(props, draft => {
+      draft.collection.items[0].story.headline = null;
+      draft.collection.items[0].story.alternative = null;
+      draft.collection.items[1].story.headline = null;
+      draft.collection.items[1].story.alternative = null;
+    });
+    return <FiveStoriesOneAdBundle {...newProps} />;
+  })
+  .add("when story slug is missing", () => {
+    const newProps = produce(props, draft => {
+      draft.collection.items[0].story.url = null;
+      draft.collection.items[1].story.url = null;
+    });
+    return <FiveStoriesOneAdBundle {...newProps} />;
+  });
