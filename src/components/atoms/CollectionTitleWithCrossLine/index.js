5c53991ad43745d32fde0cf4e1325ae770cea224
Chetan Kumar <chetan.kumar@quintype.com>
2019-07-29 16:18:39 +0530

    make default alignment 1fr 1fr 1fr

--- src/components/atoms/CollectionTitleWithCrossLine/index.js
+++ src/components/atoms/CollectionTitleWithCrossLine/index.js
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 
 import "./collectionTitleWithCrossLine.m.css";
 
-const CollectionTitleWithCrossLine = ({ title, className = "", placement = "middle", opacity }) => {
+const CollectionTitleWithCrossLine = ({ title, className = "", placement = "1fr 1fr 1fr", opacity }) => {
   if (!title) {
     return null;
   }
 
   return (
     <h2
-      styleName={`title ${placement === "middle" ? "aligned-center" : "customised-offset"}`}
+      styleName={`title customised-alignment`}
       className={className}
       style={{ "--custom-offset": placement, "--opacity": opacity || "1" }}
     >
