a0ac7c7e8673eb62b6e36eb25b29bb1eb500c023
Chetan Kumar <chetan.kumar@quintype.com>
2019-08-01 17:14:20 +0530

    Dont need qt.config

--- src/components/atoms/FullWidthSearch/stories.js
+++ src/components/atoms/FullWidthSearch/stories.js
@@ -1,13 +1,5 @@
 import React from "react";
 import FullWidthSearch from "./";
 import { withStore } from "../../../../storybook";
-import "../../../../assets/stylesheets/app.scss";
 
-withStore("Rows/FullWidthSearch", {
-  qt: {
-    config: {
-      "cdn-image": "thumbor-stg.assettype.com",
-      iconSpritePath: "/sprite.svg"
-    }
-  }
-}).add("default", () => <FullWidthSearch isOpen={true} />);
+withStore("Rows/FullWidthSearch", {}).add("default", () => <FullWidthSearch isOpen={true} />);
