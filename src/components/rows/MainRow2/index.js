a30f8b99fea5c4ccc6eb68474ed2b21e189c7e53
Supreeth R <supreethr726@gmail.com>
2019-08-02 14:34:33 +0530

    fix: pr changes made

--- src/components/rows/MainRow2/index.js
+++ src/components/rows/MainRow2/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+
 import SliderMain from "../../atoms/SliderMain";
 import SmallStoryCard from "../../atoms/SmallStoryCard";
 import CollectionBundleItem from "../../atoms/CollectionBundleItem";
