c0d333c91cdde598f5df61a1a16475b4be165486
Chetan Kumar <chetan.kumar@quintype.com>
2019-08-09 16:40:57 +0530

    Import the components in the package

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,35 @@
 import babel from "rollup-plugin-babel";
 import postcss from "rollup-plugin-postcss";
+import image from "rollup-plugin-image";
+import svgSprite from "rollup-plugin-svg-sprite";
+import sass from "rollup-plugin-sass";
+import resolve from "rollup-plugin-node-resolve";
 import packageJson from "./package.json";
-const dependencies = packageJson.dependencies || {};
 
+const dependencies = packageJson.dependencies || {};
 const commonConfig = {
-  input: "src/index.js",
+  input: "./src/index.js",
   plugins: [
+    resolve({
+      mainFields: ["module", "main", "index"],
+      modulesOnly: true,
+      browser: true,
+      customResolveOptions: {
+        moduleDirectory: "js_modules"
+      }
+    }),
+    image(),
+    sass(),
     babel({
       exclude: "node_modules/**",
-      runtimeHelpers: true
+      runtimeHelpers: true,
+      presets: [["@babel/env", { modules: false }], "@babel/preset-react"]
     }),
     postcss({
       modules: true
+    }),
+    svgSprite({
+      outputFolder: "assets/"
     })
   ],
   external: file => Object.keys(dependencies).includes(file) || file.startsWith("babel-plugin-react-css-modules")
---
+++ src/index.js
@@ -0,0 +1,143 @@
+import FourStory4s from "./components/rows/FourStory4s";
+import BigSliderSlide from "./components/atoms/BigSliderSlide";
+import BigStoryCard from "./components/atoms/BigStoryCard";
+import BigStoryCardAutoSlider from "./components/atoms/BigStoryCardAutoSlider";
+import BigStoryCardDifferentAspect from "./components/atoms/BigStoryCardDifferentAspect";
+import Carousel from "./components/atoms/Carousel";
+import CollectionBundleItem from "./components/atoms/CollectionBundleItem";
+import CollectionTitleWithCrossLine from "./components/atoms/CollectionTitleWithCrossLine";
+import Contributor from "./components/atoms/Contributor";
+import FocusedCard from "./components/atoms/FocusedCard";
+import FullWidthSearch from "./components/atoms/FullWidthSearch";
+import Headline from "./components/atoms/Headline";
+import Icon from "./components/atoms/Icon";
+import MainSliderCard from "./components/atoms/MainSliderCard";
+import ReadMoreLink from "./components/atoms/ReadMoreLink";
+import ResponsiveImageWithFallback from "./components/atoms/ResponsiveImageWithFallback";
+import SlideBigStoryCard from "./components/atoms/SlideBigStoryCard";
+import SliderMain from "./components/atoms/SliderMain";
+import SmallStoryCard from "./components/atoms/SmallStoryCard";
+import SmallStoryCardDesktopMedium from "./components/atoms/SmallStoryCardDesktopMedium";
+import SmallStoryCardDesktopMedium16x9 from "./components/atoms/SmallStoryCardDesktopMedium16x9";
+import SmallStoryCardDesktopVertical from "./components/atoms/SmallStoryCardDesktopVertical";
+import SmallStoryCardNoImageNumber from "./components/atoms/SmallStoryCardNoImageNumber";
+import SmallStoryDesktopBig from "./components/atoms/SmallStoryDesktopBig";
+import SmallStoryDesktopBigNoBorder from "./components/atoms/SmallStoryDesktopBigNoBorder";
+import SmallStoryDesktopSmallVertical from "./components/atoms/SmallStoryDesktopSmallVertical";
+import StoryCard from "./components/atoms/StoryCard";
+import StoryCardAspectRatioOne from "./components/atoms/StoryCardAspectRatioOne";
+import StoryCardDark from "./components/atoms/StoryCardDark";
+import StoryCardDesktop16IsTo9 from "./components/atoms/StoryCardDesktop16IsTo9";
+import StoryCardDesktopHover from "./components/atoms/StoryCardDesktopHover";
+import StoryCardFeaturedBig from "./components/atoms/StoryCardFeaturedBig";
+import StoryCardFeaturedBigHalfImg from "./components/atoms/StoryCardFeaturedBigHalfImg";
+import StoryCardFeaturedSmall from "./components/atoms/StoryCardFeaturedSmall";
+import StoryCardFullWidth from "./components/atoms/StoryCardFullWidth";
+import StoryCardGradientBackground from "./components/atoms/StoryCardGradientBackground";
+import StoryCardHeadlineOverlay from "./components/atoms/StoryCardHeadlineOverlay";
+import StoryCardML3x2DP9x5 from "./components/atoms/StoryCardML3x2DP9x5";
+import StoryCardML4x3DP1x1 from "./components/atoms/StoryCardML4x3DP1x1";
+import StoryCardML4x3DP2x1WithLabel from "./components/atoms/StoryCardML4x3DP2x1WithLabel";
+import StoryCardML4x3DP7x5HeadlineOverlay from "./components/atoms/StoryCardML4x3DP7x5HeadlineOverlay";
+import StoryCardML5x4DP1x1 from "./components/atoms/StoryCardML5x4DP1x1";
+import StoryCardML5x4DP4x3 from "./components/atoms/StoryCardML5x4DP4x3";
+import StoryCardML8x5DP7x5HeadlineOverlay from "./components/atoms/StoryCardML8x5DP7x5HeadlineOverlay";
+import StoryCardML9x5DP9x5 from "./components/atoms/StoryCardML9x5DP9x5";
+import StoryCardResponsiveSame from "./components/atoms/StoryCardResponsiveSame";
+import StoryStack from "./components/atoms/StoryStack";
+import TrendingStories from "./components/atoms/TrendingStories";
+import BreakingNews from "./components/rows/BreakingNews";
+import FiveStoriesOneAd from "./components/rows/FiveStoriesOneAd";
+import FiveStoriesOneAdBundle from "./components/rows/FiveStoriesOneAdBundle";
+import FiveStoryOneAdOneWidget from "./components/rows/FiveStoryOneAdOneWidget";
+import FourStoryHalfFeatured from "./components/rows/FourStoryHalfFeatured";
+import FourStoryPhotoGallery from "./src/components/rows/FourStoryPhotoGallery";
+import InvertedFourStoryHalfFeatured from "./src/components/rows/InvertedFourStoryHalfFeatured";
+import MainRow from "./src/components/rows/MainRow";
+import MainRow2 from "./src/components/rows/MainRow2";
+import NineteenStoriesOneAd from "./src/components/rows/NineteenStoriesOneAd";
+import OneCarouselTwoStoriesOneAdOneWidget from "./src/components/rows/OneCarouselTwoStoriesOneAdOneWidget";
+import SevenStory7s from "./src/components/rows/SevenStory7s";
+import SixColumnGrid from "./src/components/rows/SixColumnGrid";
+import SixteenStory4c from "./src/components/rows/SixteenStory4c";
+import SliderFocusedCard from "./src/components/rows/SliderFocusedCard";
+import SliderOneStory from "./src/components/rows/SliderOneStory";
+import TenStoriesOneAdOneWidget from "./src/components/rows/TenStoriesOneAdOneWidget";
+import ThreeStoryAutoSlider from "./src/components/rows/ThreeStoryAutoSlider";
+import ThreeStorySliderRound from "./src/components/rows/ThreeStorySliderRound";
+import TwelveStoriesOneAd from "./src/components/rows/TwelveStoriesOneAd";
+import TwelveStoriesOneAdOneWidget from "./src/components/rows/TwelveStoriesOneAdOneWidget";
+import TwoCollectionFourStories from "./src/components/rows/TwoCollectionFourStories";
+
+export {
+  FourStory4s,
+  BigSliderSlide,
+  BigStoryCard,
+  BigStoryCardAutoSlider,
+  BigStoryCardDifferentAspect,
+  Carousel,
+  CollectionBundleItem,
+  CollectionTitleWithCrossLine,
+  FourStoryPhotoGallery,
+  FourStoryHalfFeatured,
+  Contributor,
+  FocusedCard,
+  FullWidthSearch,
+  Headline,
+  InvertedFourStoryHalfFeatured,
+  Icon,
+  MainRow,
+  MainRow2,
+  NineteenStoriesOneAd,
+  OneCarouselTwoStoriesOneAdOneWidget,
+  MainSliderCard,
+  ReadMoreLink,
+  ResponsiveImageWithFallback,
+  SevenStory7s,
+  SixColumnGrid,
+  SixteenStory4c,
+  SliderFocusedCard,
+  SliderOneStory,
+  SlideBigStoryCard,
+  SliderMain,
+  SmallStoryCard,
+  SmallStoryCardDesktopMedium,
+  SmallStoryCardDesktopMedium16x9,
+  SmallStoryCardDesktopVertical,
+  SmallStoryCardNoImageNumber,
+  SmallStoryDesktopBig,
+  SmallStoryDesktopBigNoBorder,
+  SmallStoryDesktopSmallVertical,
+  StoryCard,
+  StoryCardAspectRatioOne,
+  StoryCardDark,
+  StoryCardDesktop16IsTo9,
+  StoryCardDesktopHover,
+  StoryCardFeaturedBig,
+  StoryCardFeaturedBigHalfImg,
+  StoryCardFeaturedSmall,
+  StoryCardFullWidth,
+  StoryCardGradientBackground,
+  StoryCardHeadlineOverlay,
+  StoryCardML3x2DP9x5,
+  StoryCardML4x3DP1x1,
+  StoryCardML4x3DP2x1WithLabel,
+  StoryCardML4x3DP7x5HeadlineOverlay,
+  StoryCardML5x4DP1x1,
+  StoryCardML5x4DP4x3,
+  StoryCardML8x5DP7x5HeadlineOverlay,
+  StoryCardML9x5DP9x5,
+  StoryCardResponsiveSame,
+  StoryStack,
+  TenStoriesOneAdOneWidget,
+  ThreeStoryAutoSlider,
+  ThreeStorySliderRound,
+  TwelveStoriesOneAd,
+  TwelveStoriesOneAdOneWidget,
+  TwoCollectionFourStories,
+  TrendingStories,
+  BreakingNews,
+  FiveStoriesOneAd,
+  FiveStoriesOneAdBundle,
+  FiveStoryOneAdOneWidget
+};
